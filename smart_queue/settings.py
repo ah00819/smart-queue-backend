4127ad7a65158027b0922d7073865f4ac7b144c0
ah00819 <ah0567272420@gmail.com>
2026-01-29 05:47:02 +0200

    Updated settings config.

--- smart_queue/settings.py
+++ smart_queue/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         # NOTE: This is be temprary; during development
         "HOST": "localhost",
         "USER": "root",
-        "PASSWORD": "Myroot$21#",
+        "PASSWORD": "root",
     }
 }
 
